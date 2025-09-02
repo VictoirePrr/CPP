cc3fd36397c36fda9b6892fff68574a4474f1d31
Victoire PERRIER <victoire@vicperri.home>
2025-09-02 19:27:00 +0200

    ADD : cpp05 | TO DO : ex00

---
+++ CPP_05/ex00/Bureaucrat.cpp
@@ -0,0 +1,20 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {
+
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) {
+
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+    if (this != &other) {
+
+    }
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+}
+
