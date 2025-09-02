9e945b9684827acf3bf80b667f20442a9b8b4832
Victoire PERRIER <134929082+VictoirePrr@users.noreply.github.com>
2025-09-02 19:27:00 +0200

    ADD : cpp05 | TO DO : ex00

---
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,21 @@
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat {
+
+        private :
+
+
+        public :
+
+            Bureaucrat();
+            Bureaucrat(const Bureaucrat& other);
+            Bureaucrat& operator=(const Bureaucrat& other);
+            ~Bureaucrat();
+
+};
+
+#endif // BUREAUCRAT_HPP
