e67e50f5e7c051d0fb0cbbf31f8a8bc215bb6e38
Victoire PERRIER <victoire@vicperri.home>
2025-09-03 17:13:02 +0200

    ADD : ex00 ex01 CPP05 | TO DO : finish .cpp of Form and Bureaucrat in ex01

---
+++ CPP_05/ex01/Form.cpp
@@ -0,0 +1,20 @@
+#include "Form.hpp"
+
+Form::Form() {
+
+}
+
+Form::Form(const Form& other) {
+
+}
+
+Form& Form::operator=(const Form& other) {
+    if (this != &other) {
+
+    }
+    return *this;
+}
+
+Form::~Form() {
+}
+
