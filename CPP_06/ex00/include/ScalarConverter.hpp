074dff5819784b08633ce54032ac5a9d157a65d0
Victoire PERRIER <134929082+VictoirePrr@users.noreply.github.com>
2025-10-01 13:38:31 +0200

    refactor(ex00): update ScalarConverter methods and improve type conversion logic

--- CPP_06/ex00/include/ScalarConverter.hpp
+++ CPP_06/ex00/include/ScalarConverter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vicperri <vicperri@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: victoire <victoire@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 16:46:04 by victoire          #+#    #+#             */
-/*   Updated: 2025/09/24 15:50:00 by vicperri         ###   ########lyon.fr   */
+/*   Updated: 2025/10/01 13:33:16 by victoire         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,13 @@ class ScalarConverter {
             ~ScalarConverter();
 
             static void convert(std::string &s);
-            static bool stringToDouble(const std::string& s, double& result);
-            static void doubleToInt(double& result);
-            static void doubleToChar(double& result);
+            static bool convertToDouble(const std::string &s, bool print);
+            static bool convertToFloat(const std::string &s, bool print);
+            static bool convertToInt(const std::string &s, bool print);
+            static bool convertToChar(const std::string &s, bool print);
             static bool handleSpecialString(const std::string& s);
+            static std::string findLiteralType(std::string &value);
+            static bool ssCheck(std::stringstream &ss);
             
 
 };
--- CPP_06/ex00/src/ScalarConverter.cpp
+++ CPP_06/ex00/src/ScalarConverter.cpp
@@ -3,25 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vicperri <vicperri@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: victoire <victoire@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 16:46:11 by victoire          #+#    #+#             */
-/*   Updated: 2025/09/29 12:36:22 by vicperri         ###   ########lyon.fr   */
+/*   Updated: 2025/10/01 13:37:34 by victoire         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-
-ScalarConverter::ScalarConverter() {
-}
-
-ScalarConverter::ScalarConverter(const ScalarConverter& other) {
+ScalarConverter::ScalarConverter(const ScalarConverter &other) {
     (void)other;
 }
 
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
-    if (this != &other) {
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {
+    if (this != &other)
+    {
         (void)other;
     }
     return *this;
@@ -30,60 +27,128 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 ScalarConverter::~ScalarConverter() {
 }
 
-
-bool ScalarConverter::handleSpecialString(const std::string& s) {
-    if (s == "nan" || s == "nanf" || s == "-inf" || s == "+inf" || s == "inf" 
-        || s == "inff" || s == "+inff" || s == "-inff") {
-
-        std::cout << "char : impossible" << std::endl;
-        std::cout << "int : : impossible" << std::endl;
-        std::cout << "float : " << ((s == "nan" || s == "nanf") ? "nanf" 
-        : (s == "inf" || s == "+inf" || s == "inff" || s == "+inff") ? "+inff" 
-        : (s == "-inf" || s == "-inff") ? "-inff"
-        : "") << std::endl;
-        std::cout << "double : " << ((s == "nan" || s == "nanf") ? "nan" 
-        : (s == "inf" || s == "+inf" || s == "inff" || s == "+inff") ? "+inf" 
-        : (s == "-inf" || s == "-inff") ? "-inf"
-        : "") << std::endl;
-        
-        return(true);
-    }
-    return(false);
-}
-
-void ScalarConverter::doubleToChar(double& result) {
-    
-    if (result >= CHAR_MAX || result <= CHAR_MIN)
-        std::cout << "char : impossible" << std::endl;
-    else if ((static_cast<char>(result) >= 0 && static_cast<char>(result) <= 32 ) 
-    || static_cast<char>(result) == 127)
-        std::cout << "char : Non displayable " << std::endl;
-    else
-        std::cout << "char : " << static_cast<char>(result) << std::endl;
-}
-
-void ScalarConverter::doubleToInt(double& result) {
-    if (result >= INT_MAX || result <= INT_MIN)
-        std::cout << "int : impossible" << std::endl;
-    else
-        std::cout << "int : " << static_cast<int>(result) << std::endl;
-}
-
-
-bool ScalarConverter::stringToDouble(const std::string& s, double& result) {
-    
-    std::stringstream ss(s);
-    ss >> result;
-
+bool ScalarConverter::ssCheck(std::stringstream &ss) {
     if (ss.fail()) {
         return false;
     }
-
     char nextChar = ss.peek();
     if (nextChar != EOF && nextChar != 'f') {
         return false;
     }
+    return true;
+}
+
+bool ScalarConverter::handleSpecialString(const std::string &s)
+{
+    if (s == "nan" || s == "nanf" || s == "-inf" || s == "+inf" || s == "inf" || s == "inff" || s == "+inff" || s == "-inff")
+    {
+
+        std::cout << "char : impossible" << std::endl;
+        std::cout << "int : : impossible" << std::endl;
+        std::cout << "float : " << ((s == "nan" || s == "nanf") ? "nanf" : (s == "inf" || s == "+inf" || s == "inff" || s == "+inff") ? "+inff"
+                                                                       : (s == "-inf" || s == "-inff")                                ? "-inff"
+                                                                                                                                      : "")
+                  << std::endl;
+        std::cout << "double : " << ((s == "nan" || s == "nanf") ? "nan" : (s == "inf" || s == "+inf" || s == "inff" || s == "+inff") ? "+inf"
+                                                                       : (s == "-inf" || s == "-inff")                                ? "-inf"
+                                                                                                                                      : "")
+                  << std::endl;
+
+        return (true);
+    }
+    return (false);
+}
+
+bool ScalarConverter::convertToChar(const std::string &s, bool print) {
+
+    char result;
+
+    std::stringstream ss(s);
+    ss >> result;
+
+    if (ssCheck(ss) == false)
+        return (false);
+
+    if (result >= CHAR_MAX || result <= CHAR_MIN) {
+        if (print) {
+            std::cout << "char : impossible" << std::endl;
+            convertToInt(s, false);
+            convertToFloat(s, false);
+            convertToDouble(s, false);
+        }
+        else
+            std::cout << "char : impossible" << std::endl;
+    }
+    else if ((static_cast<char>(result) >= 0 && static_cast<char>(result) <= 32) 
+    || static_cast<char>(result) == 127) {
+         if (print) {
+            std::cout << "char : Non displayable " << std::endl;
+            convertToInt(s, false);
+            convertToFloat(s, false);
+            convertToDouble(s, false);
+        }
+        else
+            std::cout << "char : Non displayable " << std::endl;
+        
+    }
     
+    else {
+        if (print) {
+            std::cout << "char : " << static_cast<char>(result) << std::endl;
+            convertToInt(s, false);
+            convertToFloat(s, false);
+            convertToDouble(s, false);
+        }
+        else
+            std::cout << "char : " << static_cast<char>(result) << std::endl;
+    }
+    return true;
+}
+
+bool ScalarConverter::convertToInt(const std::string &s, bool print) {
+
+    int result;
+
+    std::stringstream ss(s);
+    ss >> result;
+
+    if (ssCheck(ss) == false)
+        return (false);
+
+    if (result >= INT_MAX || result <= INT_MIN) {
+        if (print)  {
+            convertToChar(s, false);
+            std::cout << "int : impossible" << std::endl;
+            convertToFloat(s, false);
+            convertToDouble(s, false);
+        }
+        else
+            std::cout << "int : impossible" << std::endl;
+    }
+    
+    else {
+        if (print) {
+            convertToChar(s, false);
+            std::cout << "int : " << static_cast<int>(result) << std::endl;
+            convertToFloat(s, false);
+            convertToDouble(s, false);
+        }
+        else
+            std::cout << "int : " << static_cast<int>(result) << std::endl;
+    }
+    return true;
+}
+
+bool ScalarConverter::convertToFloat(const std::string &s, bool print) {
+
+    float result;
+
+    std::stringstream ss(s);
+    ss >> result;
+
+    if (ssCheck(ss) == false)
+        return (false);
+
     if (std::isnan(result))
         handleSpecialString("nan");
     else if (std::isinf(result)) {
@@ -91,41 +156,119 @@ bool ScalarConverter::stringToDouble(const std::string& s, double& result) {
             handleSpecialString("-inf");
         else
             handleSpecialString("+inf");
-        } 
-    else {
-        doubleToChar(result);
-        doubleToInt(result);
-        std::cout << "float : " << std::fixed << std::setprecision(1) << static_cast<float>(result) << "f" << std::endl;
-        std::cout << "double : " << std::fixed << std::setprecision(1) << result << std::endl;
     }
-        
+    
+    else {
+        if (print) {
+            convertToChar(s, false);
+            convertToInt(s, false);
+            std::cout << "float : " << std::fixed << std::setprecision(1) << static_cast<float>(result) << "f" << std::endl;
+            convertToDouble(s, false);
+        }
+        else
+            std::cout << "float : " << std::fixed << std::setprecision(1) << static_cast<float>(result) << "f" << std::endl;
+    }
+    return true;
+}
+
+bool ScalarConverter::convertToDouble(const std::string &s, bool print) {
+
+    double result;
+    std::stringstream ss(s);
+    ss >> result;
+
+    if (ssCheck(ss) == false)
+        return (false);
+
+    if (std::isnan(result))
+        handleSpecialString("nan");
+    else if (std::isinf(result)) {
+        if (std::signbit(result))
+            handleSpecialString("-inf");
+        else
+            handleSpecialString("+inf");
+    }
+    
+    else {
+        if (print) {
+            convertToChar(s, false);
+            convertToInt(s, false);
+            convertToFloat(s, false);
+            std::cout << "double : " << std::fixed << std::setprecision(1) << result << std::endl;
+        }
+        else
+            std::cout << "double : " << std::fixed << std::setprecision(1) << result << std::endl;
+    }
     return true;
 }
 
 void ScalarConverter::convert(std::string &s) {
-
-    double value;
+    
+    int i = 0;
+    enum lvl { Char, Int, Float, Double };
+    std::string compare[] = { "Char", "Int", "Float", "Double" };
+    
     if (handleSpecialString(s))
         return ;
-        //findtypeliteral(std::string &value)
-        // {
-        //    size_t pos = value.find('.');
-    //     if (pos != std::string::npos) {
-    //          size_t pos = value.find('f');
-    //       if (pos != std::string::npos)
-    //                 std::string type = float;
-    //        else
-    //             std::string type = double;
-    //}
+
+    std::string type = findLiteralType(s);
     
-    // else
-    //     std::cout << "Letter not found." << std::endl;
-    if (stringToDouble(s, value) == false) {
+    if (type.empty())
         throw ImpossibleConversion();
+
+    while (i < 4) {
+        if (compare[i] == type)
+            break;
+        i++;
     }
 
+    switch (i) {
+
+        case Char: convertToChar(s, true);
+            break;
+        case Int: convertToInt(s, true);
+            break;
+        case Float: convertToFloat(s, true);
+            break;
+        case Double: convertToDouble(s, true);
+        default:
+            throw ImpossibleConversion();
+            break;
+    }
 }
 
-const char* ScalarConverter::ImpossibleConversion::what() const throw() {
-	return "char : impossible\nint : : impossible\nfloat : impossible\ndouble : impossible";
+std::string ScalarConverter::findLiteralType(std::string &value) {
+
+    for (int i = 0; value[i]; i++){
+        if (!std::isprint(value[i]))
+            return (NULL);
+    }
+
+    for (int i = 0; value[i]; i++) {
+        if (!std::isalpha(value[i]))
+            break;
+        if (i == value.length())
+            ;
+        return ("Char");
+    }
+
+    for (int i = 0; value[i]; i++) {
+        if (!std::isdigit(value[i]))
+            return (NULL);
+    }
+
+    size_t pos = value.find('.');
+    if (pos != std::string::npos)
+        size_t pos = value.find('f');
+    else
+        return ("Int");
+
+    if (pos != std::string::npos)
+        return ("Float");
+    else
+        return ("Double");
+}
+
+const char *ScalarConverter::ImpossibleConversion::what() const throw() {
+    return "char : impossible\nint : : impossible\nfloat : impossible\ndouble : impossible";
 }
