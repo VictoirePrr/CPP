e4568b1a0841eaa3e9b1af05c1dff41e9d44d82c
Victoire PERRIER <134929082+VictoirePrr@users.noreply.github.com>
2025-10-27 10:01:44 +0100

    refractor: removed whatever before swap function

--- CPP_07/ex00/src/main.cpp
+++ CPP_07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vicperri <vicperri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 12:11:20 by vicperri          #+#    #+#             */
-/*   Updated: 2025/10/07 12:19:09 by vicperri         ###   ########lyon.fr   */
+/*   Updated: 2025/10/27 10:00:32 by vicperri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ int main(void)
 {
     int a = 2;
     int b = 3;
-    Whatever::swap(a, b);
+    ::swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << Whatever::min(a, b) << std::endl;
-    std::cout << "max( a, b ) = " << Whatever::max(a, b) << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
     std::string c = "chaine1";
     std::string d = "chaine2";
-    Whatever::swap(c, d);
+    ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << Whatever::min(c, d) << std::endl;
-    std::cout << "max( c, d ) = " << Whatever::max(c, d) << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 }
