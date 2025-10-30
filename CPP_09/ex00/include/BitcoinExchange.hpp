aa857e31799fa10ed4a8fd8a663aa81b2d29e2bb
Victoire PERRIER <134929082+VictoirePrr@users.noreply.github.com>
2025-10-30 15:01:37 +0100

    feat(CPP09): add BitcoinExchange class with basic structure and Makefile setup

---
+++ CPP_09/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+
+class BitcoinExchange {
+
+        private :
+
+
+        public :
+
+            BitcoinExchange();
+            BitcoinExchange(const BitcoinExchange& other);
+            BitcoinExchange& operator=(const BitcoinExchange& other);
+            ~BitcoinExchange();
+
+};
+
+#endif // BITCOINEXCHANGE_HPP
---
+++ CPP_09/ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,20 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {
+
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {
+
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
+    if (this != &other) {
+
+    }
+    return *this;
+}
+
+BitcoinExchange::~BitcoinExchange() {
+}
+
