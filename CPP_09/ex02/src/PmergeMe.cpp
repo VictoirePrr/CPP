d3c1bb0e60d0701e1237d1d1bbe1a3533271e03e
Victoire PERRIER <134929082+VictoirePrr@users.noreply.github.com>
2025-11-17 18:17:07 +0100

    feat(ex02): complete vectorPairs function with possibility of odd numbers

--- CPP_09/ex02/src/PmergeMe.cpp
+++ CPP_09/ex02/src/PmergeMe.cpp
@@ -1,90 +1,117 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe() {
-
+PmergeMe::PmergeMe()
+{
 }
 
-PmergeMe::PmergeMe(const PmergeMe& other) {
- (void)other;
+PmergeMe::PmergeMe(const PmergeMe &other)
+{
+    (void)other;
 }
 
-PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
-    if (this != &other) {
-
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+    if (this != &other)
+    {
     }
     return *this;
 }
 
-PmergeMe::~PmergeMe() {
+PmergeMe::~PmergeMe()
+{
 }
 
-
-
-int PmergeMe::parseArgs(char *args) {
-    char* endptr;
+int PmergeMe::parseArgs(char *args)
+{
+    char *endptr;
     errno = 0;
 
     long num = std::strtol(args, &endptr, 10);
-    if (endptr == args) {
-        return(1);
+    if (endptr == args)
+    {
+        return (1);
     }
-    else if (*endptr != '\0') {
-        return(1);
+    else if (*endptr != '\0')
+    {
+        return (1);
     }
-    if (num < 0 || num >= INT_MAX || errno == ERANGE) {
-        return(1);
+    if (num < 0 || num >= INT_MAX || errno == ERANGE)
+    {
+        return (1);
     }
-        
+
     vec.push_back(static_cast<int>(num));
     deq.push_back(static_cast<int>(num));
-    return(0);
+    return (0);
 }
 
-int PmergeMe::checkDuplicates() {
+int PmergeMe::checkDuplicates()
+{
 
-      std::set<int> s(vec.begin(), vec.end());
-        if (s.size() == vec.size())
-            return(0);
-    return(1);
+    std::set<int> s(vec.begin(), vec.end());
+    if (s.size() == vec.size())
+        return (0);
+    return (1);
 }
 
-void PmergeMe::vectorPairs(size_t pairSize) {
+void PmergeMe::vectorPairs(size_t pairSize)
+{
 
-    printPairs(); 
+    printPairs();
     pairSize *= 2;
 
     if (pairSize > vec.size() / 2)
         return;
 
     std::vector<std::vector<int> > newPairs;
-    
+    std::vector<int> leftover;
+
     if (vecpairs.empty()) {
-        for (size_t i = 0; i + pairSize <= vec.size(); i += pairSize) {
+
+        size_t fullGroups = vec.size() / pairSize;
+        size_t takeCount = fullGroups * pairSize;
+
+        for (size_t i = 0; i + pairSize <= takeCount; i += pairSize) {
             std::vector<int> pair;
             pair.insert(pair.end(), vec.begin() + i, vec.begin() + i + pairSize);
-            swapPairs(pair); 
-            newPairs.push_back(pair);
-        }
-    } else {
-        for (size_t i = 0; i + 2 <= vecpairs.size(); i += 2) {
-            std::vector<int> pair;
-            pair.insert(pair.end(), vecpairs[i].begin(), vecpairs[i].end());
-            pair.insert(pair.end(), vecpairs[i+1].begin(), vecpairs[i+1].end());
             swapPairs(pair);
             newPairs.push_back(pair);
         }
+
+        if (takeCount < vec.size())
+            leftover.insert(leftover.end(), vec.begin() + takeCount, vec.end());
+    } else {
+
+        for (size_t i = 0; i + 2 <= vecpairs.size(); i += 2) {
+            std::vector<int> pair;
+            pair.insert(pair.end(), vecpairs[i].begin(), vecpairs[i].end());
+            pair.insert(pair.end(), vecpairs[i + 1].begin(), vecpairs[i + 1].end());
+            swapPairs(pair);
+            newPairs.push_back(pair);
+        }
+
+        if (vecpairs.size() % 2 != 0)
+            leftover = vecpairs.back();
     }
 
     vecpairs = newPairs;
-
     vectorPairs(pairSize);
+
+    if (vecpairs.empty() && !leftover.empty())
+        vecpairs.push_back(leftover);
+    else if (!vecpairs.empty() && !leftover.empty())
+        vecpairs.push_back(leftover);
+    printPairs();
 }
 
-void PmergeMe::printPairs() {
-        std::cout << "Current pairs:\n";
-    for (size_t i = 0; i < vecpairs.size(); ++i) {
+void PmergeMe::printPairs()
+{
+    std::cout << "Current pairs:\n";
+    for (size_t i = 0; i < vecpairs.size(); ++i)
+    {
         std::cout << "(";
-        for(size_t j = 0; j < vecpairs[i].size(); ++j) {
+        for (size_t j = 0; j < vecpairs[i].size(); ++j)
+        {
             std::cout << vecpairs[i][j];
             if (j < vecpairs[i].size() - 1)
                 std::cout << ", ";
@@ -94,17 +121,20 @@ void PmergeMe::printPairs() {
     std::cout << "-------\n";
 }
 
+void PmergeMe::swapPairs(std::vector<int> &pair)
+{
 
-void PmergeMe::swapPairs(std::vector<int> &pair) {
-
-     int half = pair.size() / 2;
-        for (size_t i = 0; i < pair.size(); i++) {
-            if (i + 1 == pair.size()) {
-                int &b = pair[i / 2];
-                int &a = pair[i];
-                if (b > a) {
-                    std::swap_ranges(pair.begin(), pair.begin() + half, pair.begin() + half);
-                }
+    int half = pair.size() / 2;
+    for (size_t i = 0; i < pair.size(); i++)
+    {
+        if (i + 1 == pair.size())
+        {
+            int &b = pair[i / 2];
+            int &a = pair[i];
+            if (b > a)
+            {
+                std::swap_ranges(pair.begin(), pair.begin() + half, pair.begin() + half);
             }
+        }
     }
 }
--- CPP_09/ex02/src/main.cpp
+++ CPP_09/ex02/src/main.cpp
@@ -5,10 +5,11 @@ int main(int argc, char **argv)
     PmergeMe start;
     size_t pairSize = 1;
 
-    if (argc == 2){
-            std::cout << "Error" << std::endl;
-            return(1);
-        } 
+    (void)argc;
+    // if (argc == 2){
+    //         std::cout << "Error" << std::endl;
+    //         return(1);
+    //     } 
 
     for (int i = 1; argv[i]; i++) {
         if (start.parseArgs(argv[i])) {
